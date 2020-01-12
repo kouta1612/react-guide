31cdd3ed9f1f9669eac6a57464f373ae1d9adc5b
kouta1612 <kouta1612world69@gmail.com>
2020-01-12 19:14:46 +0900

    add clock

--- src/index.js
+++ src/index.js
@@ -1,41 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Avatar(props) {
-    return (
-        <img className="Avatar"
-            src={props.user.avatarUrl}
-            alt={props.user.name}
-        />
-    );
-}
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
 
-function UserInfo(props) {
-    return (
-        <div className="UserInfo">
-            <Avatar user={props.user} />
-            <div className="UserInfo-name">
-                {props.user.name}
-            </div>
-        </div>
-    );
-}
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
 
-function Comment(props) {
-    return (
-        <div className="Comment">
-            <UserInfo user={props.author} />
-            <div className="Comment-text">
-                {props.text}
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
             </div>
-            <div className="Comment-date">
-                {props.date}
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 ReactDOM.render(
-    <Comment />,
+    <Clock />,
     document.getElementById('root')
 );
