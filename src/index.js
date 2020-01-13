1da42f2caecc544a2d0f50a6a5bdd663db0e2ceb
kouta1612 <kouta1612world69@gmail.com>
2020-01-13 18:48:42 +0900

    add toggle button for  click event

--- src/index.js
+++ src/index.js
@@ -1,40 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Clock extends React.Component {
+class Toggle extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { date: new Date() };
+        this.state = { isToggleOn: true };
     }
 
-    componentDidMount() {
-        this.timerID = setInterval(
-            () => this.tick(),
-            1000
-        );
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
-
-    tick() {
-        this.setState({
-            date: new Date()
-        })
+    handleClick = () => {
+        this.setState((state) => ({
+            isToggleOn: !state.isToggleOn
+        }));
     }
 
     render() {
         return (
-            <div>
-                <h1>Hello, world</h1>
-                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-            </div>
+            <button onClick={this.handleClick}>
+                {this.state.isToggleOn ? 'ON' : 'OFF'}
+            </button>
         );
     }
 }
 
 ReactDOM.render(
-    <Clock />,
+    <Toggle />,
     document.getElementById('root')
 );
